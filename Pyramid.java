eb35aed82e31935ae78e658346e7c257f674e3d8
mRahulGiri <75267764+mRahulGiri@users.noreply.github.com>
2020-12-01 10:29:24 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,41 @@
+package zoho.corp;
+import java.io.*; 
+
+public class Pyramid {
+	public static void printStars(int n) 
+	{ 
+		int i, j; 
+		int c=1;
+		
+		for(i=0; i<n; i++) 
+		{ 
+
+			
+			for(j=2*(n-i); j>=0; j--) 
+			{ 
+				
+				System.out.print(" "); 
+			} 
+			
+			
+			for(j=0; j<=i; j++) 
+			{ 
+				
+				System.out.print(c++ + " "); 
+			} 
+			
+			
+			System.out.println(); 
+			
+		}
+	}
+	public static void main(String[] args) {
+		
+		int n = 5; 
+		printStars(n); 
+
+	}
+	
+	
+
+}
